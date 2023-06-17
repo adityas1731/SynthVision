4f2b7aa34216b5bf786dc0d67735b8e64282567b
Aditya Vikram Singh <adityas.1731@gmail.com>
2023-06-17 11:41:46 +0530

    UI

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('https://dall-e-clone-cskh.onrender.com/api/v1/post', {
+      const response = await fetch('https://image-generation-uzo9.onrender.com/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
